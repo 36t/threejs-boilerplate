4dec187703db1db6753277224b79ba02f206ed28
36t <saluta@gmail.com>
2021-02-26 14:48:15 +0900

    del: deleted another endpoint.

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,6 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/js/app.js', // エントリポイント①
-    another: './src/js/another.js' //エントリポイント②
   },
   output: {
     path: path.resolve(__dirname, 'public'), // 絶対パス必要
@@ -120,12 +119,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/html/index.html',
       chunks: ['app'] // 読み込ませたいエントリポイント名を指定する
-    }),
-    // anotherの設定
-    new HtmlWebpackPlugin({
-      filename: 'another.html', // 出力するファイル名。何も指定していないとindex.html
-      template: './src/html/another.html', // テンプレートの場所
-      chunks: ['another'] // 読み込ませたいエントリポイント名を指定する
     }),
     // sassの設定
     new MiniCssExtractPlugin({
