ccb92711e90eb055de18ee5641d4741626864b90
36t <saluta@gmail.com>
2021-03-01 23:14:30 +0900

    feat: added images.

--- src/ts/global.d.ts
+++ src/ts/global.d.ts
@@ -11,3 +11,18 @@ declare module '*.glsl' {
   const value: string
   export default value
 }
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
