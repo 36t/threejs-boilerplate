219cf72dba4715095faf1fb96d7792cb2e137f32
36t <saluta@gmail.com>
2021-03-01 13:01:56 +0900

    del: deleted comments.

--- webpack.common.js
+++ webpack.common.js
@@ -1,46 +1,33 @@
-// 共通webpack設定ファイル
 const path = require('path');
-// 出力先をクリーンアップ
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// htmlテンプレート
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// cssの外部化
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// 出力するcssの最適化
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 
 module.exports = {
   entry: {
-    app: './src/ts/App.ts', // エントリポイント①
+    app: './src/ts/App.ts',
   },
   output: {
-    path: path.resolve(__dirname, 'public'), // 絶対パス必要
-    /* 以下2つを有効にすると、全て「bundle」がついてしまう
-    filename: 'js/[name].bundle.js', // 出力するファイルの名前([name]にentryポイントが入る。「.bundle.」は無くても良い)
-    chunkFilename: 'js/[name].js', // splitChunksの設定はこちらが優先される
-    */
+    path: path.resolve(__dirname, 'public'),
     filename: (pathData) => {
-      // pathData.chunk.name：nameが入る。ここでは「app, another, vendor, vendor-modules」
       return pathData.chunk.name.search(/vendor/) > -1 ? 'js/[name].js' : 'js/[name].[contenthash].js';
     },
   },
   optimization: {
-    // ファイルの分割で指定
     splitChunks: {
-      chunks: 'initial',  // initial:静的にインポートしているモジュールが対象(要するに「import」の部分が分割の対象になる),
+      chunks: 'initial', 
       cacheGroups: {
-        // jqueryとvelocity-animateの出力設定
-        vendorJS: { // 「vendorJS」の名前は任意で良い。わかりやすい名前を
-          test: /[\\/]node_modules[\\/]/, // jqueryとvelocity-animateはnode_modulesの下にあるのでこのように設定
-          name: 'vendor' // 分割して出力するファイルの名前
+        vendorJS: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor'
         },
-        // src/scss/sytle.scssの出力設定
-        stylesheet: { // プロパティ名は任意で良い
-          test: /[\\/]src[\\/]scss[\\/]/, // [\\\\/] <= 途中のスラッシュを書く場合 (普通に書くと、ウインドウズは動作しない)
-          name: 'style', // 分割して出力するファイルの名前
-          minSize: 0, // 分割の対象となるモジュールの最小サイズ。デフォルトは30kb。今回めっちゃ小さいので
-          minChunks: 2 // モジュールがいくつの場所で利用されていれば分割の対象とするか
+        stylesheet: { 
+          test: /[\\/]src[\\/]scss[\\/]/,
+          name: 'style',
+          minSize: 0,
+          minChunks: 2
         }
       }
     },
@@ -52,51 +39,46 @@ module.exports = {
   },
   module: {
     rules: [
-      // eslintの設定
+      // {
+      //   // enforce: 'pre', // preを指定してない場合よりも先に実行する。今回はBabelよりも先
+			// 	test: /\.tsx?$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   options: {
+      //     fix: true, // eslintloaderのオプション。場合によってはコードを調整してくれる
+      //   }
+      // },
       {
-        // enforce: 'pre', // preを指定してない場合よりも先に実行する。今回はBabelよりも先
-        test: /\\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          fix: true, // eslintloaderのオプション。場合によってはコードを調整してくれる
-        }
-      },
-      {
+        enforce: 'pre',
 				test: /\.(glsl|vs|fs)$/,
 				loader: "ts-shader-loader"
 			},
 			{
+        enforce: 'pre',
 				test: /\.tsx?$/,
 				exclude: [/node_modules/, /tsOld/],
 				loader: "ts-loader"
 			},
-      // Babelの設定
-      // {
-      //   test: /\\.js$/, // loaderの処理対象。今回はjs
-      //   exclude: /node_modules/, // 除外したいディレクトリ。node_modulesを入れると処理が重くなるので、基本的にはこの設定を行う
-      //   loader: 'babel-loader' // 利用するloader
-      // },
-      // sassについて
       {
-        // test: /\\.scss$/, // エラーになる
-        test: /\.scss$/,
-        // use: ['style-loader', 'css-loader', 'sass-loader'] // loaderは指定した順番の逆から実行される　
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'] // loaderは指定した順番の逆から実行される　
+        test: /\\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       },
-      // imageについて
       {
-        test: /\.(jpe?g|gif|png|svg)$/, // 画像の拡張子
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/,
         use: [
           {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'img', // 画像の出力先。デフォルトのパスは「path: path.resolve(__dirname, 'public')」
-              publicPath: '/img', // 出力する画像からのパス。public/imgにしたい場合は'/img'
+              outputPath: 'img', 
+              publicPath: '/img',
             },
           },
-          // 画像の圧縮
           {
             loader: 'image-webpack-loader',
             options: {
@@ -107,7 +89,6 @@ module.exports = {
           }
         ],
       },
-      // htmlについて (画像)
       {
         test: /\.html$/,
         loader: 'html-loader'
@@ -118,17 +99,15 @@ module.exports = {
         extensions: ['.ts','.tsx','.js'],
         modules: [path.resolve(__dirname, 'src'), 'node_modules']
     },
+    target: ["web", "es5"],
   plugins: [
-    // 上記outputで指定したディレクトリ以下をクリーンアップ
     new CleanWebpackPlugin(),
-    // htmlテンプレートを指定
     new HtmlWebpackPlugin({
       template: './src/html/index.html',
-      chunks: ['app'] // 読み込ませたいエントリポイント名を指定する
+      chunks: ['app']
     }),
-    // sassの設定
     new MiniCssExtractPlugin({
-      filename: './css/[name].[contenthash].css' // output.pathの相対パスに出力
+      filename: './css/[name].[contenthash].css'
     })
   ]
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,13 +5,10 @@ const path = require('path');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
-  // ソースマップの生成
   devtool: 'eval-cheap-source-map',
-  // webpack dev serverの設定
   devServer: {
-    // サーバー起動時に自動的に開く
     open: true,
     port: 9000,
-    contentBase: path.resolve(__dirname, 'public')
+    contentBase: path.resolve(__dirname, 'public'),
   }
 })
