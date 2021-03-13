8fd13c4454f140e101d0cc5f15aea6eeded7250a
36t <saluta@gmail.com>
2021-03-13 15:37:18 +0900

    feat: added compress, hot, plugins setting.

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,15 +2,19 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
   devtool: 'eval-cheap-source-map',
   devServer: {
     open: true,
-    port: 9000,
+    compress: true,
     hot: true,
-    watchContentBase: true,
-    contentBase: path.resolve(__dirname, 'public'),
-  }
+    port: 9000,
+    contentBase: path.resolve(__dirname, 'src'), // <- watch directory
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 })
